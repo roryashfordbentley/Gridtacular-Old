dfe80e05cc76e932093a3e4825ed2753ba71f020
Rory Ashford <rory@superstrukt.co.uk>
2016-02-20 21:27:08 +0000

    fixed build script again!

--- doc-gen/index.js
+++ doc-gen/index.js
@@ -29,9 +29,29 @@ Handlebars.registerPartial(
 
 // All paths relative to index.js
 
+//console.log(Metalsmith);
+
+/*function navigation(){
+    var anchorlinks =  [];
+    return function (files, metalsmith, done){
+        //Object.keys(files).forEach(function(file) {
+        for (var file in files) {
+            if (!html(file)) return;
+            var elements = file.querySelectorAll("#");
+            
+            for (i = 0; i < elements.length; ++i) {
+                anchorlinks.push(elements[i].id);  
+            }
+        }
+        console.log(files);
+        done();
+    };
+}*/
+
 Metalsmith(__dirname)
     .source("src")
     .use(markdown())
+    //.use(navigation())
     .use(templates('handlebars'))
     .use(headings('h2'))
     .use(ignore([
@@ -44,8 +64,10 @@ Metalsmith(__dirname)
     .use(permalinks({
         pattern: ':title'
     }))
-    .destination('./build')
+    .destination('../docs')
     .build(function(err) {
         if (err) console.log(err);
     }
 );
+
+console.log('metalsmith complete');
